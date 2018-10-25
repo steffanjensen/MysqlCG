b8ccbb26a66a854a22470b605ff583467b231a77
Steffan Max <brominercom@gmail.com>
2018-10-25 03:15:27 +0200

    clear and comments

--- mysql.sh
+++ mysql.sh
@@ -5,8 +5,10 @@
 # Our Github: https://github.com/reliefs/MysqlCG #
 # This script is free to use, modify and resell #
 
+clear
 echo -e "Welcome To MysqlCG\n" 
 
+# Type user information
 echo -e "What is your Username?\n"
 
 read UserName
@@ -19,22 +21,12 @@ echo -e "Select your option\n1. Login to mysql\n2. See Databases\n3. Select Data
 
 read n
 
-
+# All different options
 case $n in
-       	1) mysql -u $UserName --password=$PassWord;;
+	1) mysql -u $UserName --password=$PassWord;;
 	2) mysql -u $UserName --password=$PassWord -e 'show databases;';; 
 	3) echo -e "\nType database name\n"; read database ; mysql -u $UserName --password=$PassWord --database=$database;;
         4) mysql --version;;
-	5) echo -e "\nType database name\n"; read database ; mysql -u $UserName --password=$PassWord --database=$database -e 'show tables;';;
+	5) echo -e "\nType database name\n"; read database ; mysql -u $UserName --password=$PassWord --database=$database -e 'show tables;';echo -e "Type table name";read table;;
 	6) printf "MysqlCG is a Mysql Client Gui. MysqlCG is open-source and free to use and reproduce. I do not take any ownership of this script and you can do with it what you want.";  
 esac
-
-
-
-	
-
-
-
-
-
-
